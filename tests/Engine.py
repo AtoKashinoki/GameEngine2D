84153558c705acacedb3fc428e2ebd212f4cbbf0
AtoKashinoki <atokashinoki@gmail.com>
2024-10-17 09:13:15 +0900

    Initial branch

--- tests/Engine.py
+++ tests/Engine.py
@@ -7,13 +7,14 @@ from AppEngine.Engine import FlameEngine
 class Test(FlameEngine):
 
     def __update__(self) -> None | int:
-        print(self.frame.pressed)
         if 32 in self.frame.pressed and 101 in self.frame.pressed:
             return System.EXIT
         if 32 in self.frame.pressed and 114 in self.frame.pressed:
             return System.REBOOT
         return
-    def __render__(self) -> None: ...
+    def __render__(self) -> None:
+        print(self.frame.pressed)
+        return
 
     ...
 
